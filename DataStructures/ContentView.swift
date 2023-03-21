3043c8050308050e715c790a4d15ddc29d3daee3
Henry-Hart-School <hart.h@etoncollege.org.uk>
2023-03-21 14:28:50 +0000

    Initial Commit

---
+++ DataStructures/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DataStructures
+//
+//  Created by Hart, Henry (AGDF) on 21/03/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DataStructures/DataStructuresApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataStructuresApp.swift
+//  DataStructures
+//
+//  Created by Hart, Henry (AGDF) on 21/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct DataStructuresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
